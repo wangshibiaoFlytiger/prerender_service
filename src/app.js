ddfbb88039a21377e0f150ae97ca6231f3544d14
wangshibiao <645102170@qq.com>
2019-12-27 18:10:18 +0800

    feat(prerender_service): 验证预渲染服务
    目前存在的问题: 预渲染代理服务没有验证通过

--- src/app.js
+++ src/app.js
@@ -2,20 +2,29 @@ import {addGameData, findGameList, findPage} from "./controller/gameController";
 
 const config = require("config");
 const Koa = require('koa');
-const Router = require('@koa/router');
-
 const app = new Koa();
-const router = new Router();
 
-// 添加koa-body中间件,使可以获取request body参数
-const koaBody = require('koa-body');
 
-// 定义路由
-router.post("/game/addGameData", koaBody(), addGameData);
-router.get("/game/findGameList", findGameList);
-router.get("/game/findPage", findPage);
+/*****************start 创建prerender服务*******************************/
+const prerender = require('prerender');
+const server = prerender();
+server.start();
+/*****************end 创建prerender服务*******************************/
 
-// 添加路由中间件
-app.use(router.routes());
+/*****************start 创建prerender的代理服务*******************************/
+let koaPrerender = require('koa-prerender');
+
+// 将请求转发到prerender服务进行预渲染, 如访问http://localhost:3000/http://www.baidu.com, 或返回http://www.baidu.com的内容
+const PRERENDER_SERVER_URL = "http://localhost:3000/";
+let options = {
+    prerender: PRERENDER_SERVER_URL,   // optional, default:'http://service.prerender.io/'
+    // protocol: 'http',                 // optional, default: this.protocol
+    // host: 'localhost:8081',      // optional, default: this.host,
+    // prerenderToken: ''                // optional or process.env.PRERENDER_TOKEN
+};
+
+// Use as middleware
+app.use(koaPrerender(options));
 
 app.listen(config.get("service.port"));
+/*****************end 创建prerender的代理服务*******************************/
